75bcaa88152af18ba084310630cc5dc783af5201
Padraig O Neill <contact.padraig.o.neill@gmail.com>
2020-03-31 00:27:08 +0100

    Finished UI rock paper scissors

--- js/script.js
+++ js/script.js
@@ -2,91 +2,110 @@ const rock = document.createElement("button");
 const paper = document.createElement("button");
 const scissors = document.createElement("button");
 
+const resultsDiv = document.createElement("div");
 
-rock.textContent = 'ROCK';
+const playerSelectionP = document.createElement("p");
+const computerSelectionP = document.createElement("p");
+const roundP = document.createElement("p");
+const runningTotalSectionP = document.createElement("p");
+const gameNumberP = document.createElement("p");
+const playerWonGamesP = document.createElement("p");
+const computerWonGamesP = document.createElement("p");
 
-paper.textContent = 'PAPER';
-
-scissors.textContent = 'SCISSORS';
+rock.textContent = "Rock";
+paper.textContent = "Paper";
+scissors.textContent = "Scissors";
 
 container.appendChild(rock);
 container.appendChild(paper);
 container.appendChild(scissors);
 
-buttons = container.getElementsByClassName("button");
-buttons.addEventListener("click", function(event){
-   console.log(event);
+playerSelectionP.textContent = "Player Selection";
+computerSelectionP.textContent = "Computer Selection";
+roundP.textContent = "Round Results";
+runningTotalSectionP.textContent = "Running Total";
+gameNumberP.textContent = "Game Number: ";
+playerWonGamesP.textContent = "Player Won : ";
+computerWonGamesP.textContent = "Computer Won: ";
+
+resultsDiv.appendChild(playerSelectionP);
+resultsDiv.appendChild(computerSelectionP);
+resultsDiv.appendChild(roundP);
+resultsDiv.appendChild(runningTotalSectionP);
+resultsDiv.appendChild(gameNumberP);
+resultsDiv.appendChild(playerWonGamesP);
+resultsDiv.appendChild(computerWonGamesP);
+
+container.append(resultsDiv);
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll("button");
+var playerSelection = "";
+
+playerWins = 0;
+computerWins = 0;
+gamesPlayed = 0;
+
+// we use the .forEach method to iterate through each button
+buttons.forEach(button => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", e => {
+    console.log(playerSelection);
+    playerSelection = button.textContent;
+
+    console.log(playerSelection);
+    result = playRound(button.textContent, computerPlay());
+    if (result == "playerWon") {
+      roundP.textContent = "Player Wins";
+      playerWins++;
+      gamesPlayed++;
+    } else if (result == "computerWon") {
+      roundP.textContent = "Player Loses";
+      computerWins++;
+      gamesPlayed++;
+    } else {
+      roundP.textContent = "Its a draw";
+      playerWins++;
+      computerWins++;
+      gamesPlayed++;
+    }
+    playerWonGamesP.textContent = "Player Won: " + playerWins;
+    computerWonGamesP.textContent = "Computer Won: " + computerWins;
+    gameNumberP.textContent = "Game Number: " + gamesPlayed;
+
+    if (gamesPlayed == 5) {
+      if (playerWins > computerWins)
+        runningTotalSectionP.textContent = "Player is the grand champ";
+      else if (playerWins < computerWins)
+        runningTotalSectionP.textContent = "Computer is the grand champ";
+      else runningTotalSectionP.textContent = "You both suck";
+    }
+  });
 });
 
-
-
-
-// function playSound(e){
-//   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-//   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
-//   if (!audio) return;
-//   audio.currentTime =0;
-//   audio.play()
-//   console.log(key)
-//   key.classList.add('playing')
-// }
-
-// function removeTransition(e) {
-// if(e.propertyName !== 'transform' )
-// this.classList.remove('playing');
-
-// }
-
-
 let computerPlay = function() {
-  let choices = ["rock", "paper", "scissors"];
-
+  let choices = ["Rock", "Paper", "Scissors"];
   let choice = choices[Math.floor(Math.random() * choices.length)];
   return choice;
 };
 
 function playRound(playerSelection, computerSelection) {
+  playerSelectionP.textContent = "Player selected: " + playerSelection;
+  computerSelectionP.textContent = "Computer selected: " + computerSelection;
+
   playerChoice = playerSelection.toLowerCase();
+  computerChoice = computerSelection.toLowerCase();
 
-  if (playerChoice == computerSelection) {
-    return "Its a draw";
-  }
-  if (playerChoice == "rock") {
-    if (computerSelection == "scissors") return "Player Wins";
-    else return "Player Loses";
-  }
-
-  if (playerChoice == "paper") {
-    if (computerSelection == "rock") return "Player Wins";
-    else return "Player Loses";
-  }
-
-  if ((playerChoice = "scissors")) {
-    if (computerSelection == "paper") return "Player Wins";
-    else return "Player Loses";
+  if (playerChoice == computerChoice) {
+    return "draw";
+  } else if (playerChoice == "rock") {
+    if (computerChoice == "scissors") return "playerWon";
+    return "computerWon";
+  } else if (playerChoice == "paper") {
+    if (computerChoice == "rock") return "playerWon";
+    return "computerWon";
+  } else if (playerChoice == "scissors") {
+    if (computerChoice == "paper") return "playerWon";
+    return "computerWon";
   }
 }
-
-let game = function() {
-  playerWins = 0;
-  computerWins = 0;
-  var i = 0;
-
-  // while (i < 5) {
-  let playerSelection = prompt("What is your choice?");
-  result = playRound(playerSelection, computerPlay());
-  if (result == "Its a draw") {
-    playerWins++;
-    computerWins++;
-  } else if (result == "Player Wins") playerWins++;
-  else computerWins++;
-  console.log(result);
-  // i++;
-  // }
-  if (playerWins > computerWins) console.log("Player is the grand champ");
-  else if (playerWins < computerWins)
-    console.log("Computer is the grand champ");
-  else console.log("You both suck");
-};
-
-game();
